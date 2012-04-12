908b54062c4bdce4f2c989c0409fe32f98e95a62
tangyue <tangyue1004@gmail.com>
2012-04-12 15:07:46 +0800

    add ViewFlipper

--- src/motion/event/Card.java
+++ src/motion/event/Card.java
@@ -11,46 +11,44 @@ import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
-
-public class Card extends View{
-
+public class Card extends View {
 
 	public Canvas canvas;
 	public Paint p;
 	private Bitmap bitmap;
-	float x,y;
+	float x, y;
 	public Path path;
 
-	public Card(Context context, AttributeSet attrs){
+	public Card(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		             
-		canvas=new Canvas();      
+
+		canvas = new Canvas();
 		p = new Paint(Paint.DITHER_FLAG);
-		p.setAntiAlias(true);                
-		p.setColor(Color.RED);              
-		p.setStrokeCap(Paint.Cap.ROUND);   
+		p.setAntiAlias(true);
+		p.setColor(Color.RED);
+		p.setStrokeCap(Paint.Cap.ROUND);
 		p.setPathEffect(new CornerPathEffect(50));
-		p.setStrokeWidth(8);  
+		p.setStrokeWidth(8);
 		p.setStyle(Paint.Style.STROKE);
-		path=new Path();
-		
+		path = new Path();
+
 	}
-	
+
 	@Override
-	public boolean onTouchEvent(MotionEvent event){
+	public boolean onTouchEvent(MotionEvent event) {
 		int action = event.getAction();
-		switch(action){
+		switch (action) {
 
 		case MotionEvent.ACTION_DOWN:
-			x = event.getX();				
+			x = event.getX();
 			y = event.getY();
-			canvas.drawPoint(x, y, p); 
+			canvas.drawPoint(x, y, p);
 			path.moveTo(x, y);
 			invalidate();
 			break;
 
 		case MotionEvent.ACTION_MOVE:
-		
+
 			path.lineTo(x, y);
 			canvas.drawPath(path, p);
 			invalidate();
@@ -60,34 +58,36 @@ public class Card extends View{
 			break;
 		case MotionEvent.ACTION_CANCEL:
 			break;
-		
+
 		}
-		//event.recycle();
-		x = event.getX();   
+		// event.recycle();
+		x = event.getX();
 		y = event.getY();
 		return true;
-		// return super.onTouchEvent(event); 
+		// return super.onTouchEvent(event);
 	}
 
-	public void clean(){
+	public void clean() {
 		path.rewind();
-		bitmap = Bitmap.createBitmap(this.canvas.getWidth(), this.canvas.getHeight(), Bitmap.Config.ARGB_8888);
-		canvas.setBitmap(bitmap); 
+		bitmap = Bitmap.createBitmap(this.canvas.getWidth(),
+				this.canvas.getHeight(), Bitmap.Config.ARGB_8888);
+		canvas.setBitmap(bitmap);
 		this.canvas.drawBitmap(bitmap, 0, 0, null);
 		invalidate();
-		
+
 	}
-	
+
 	@Override
 	public void draw(Canvas c) {
-		if(bitmap==null){
-			bitmap = Bitmap.createBitmap(c.getWidth(), c.getHeight(), Bitmap.Config.ARGB_8888);
-			//canvas.drawRect(new Rect(c.getWidth()/2,0,0,0), p); 
-			//canvas=new Canvas();         
-			canvas.setBitmap(bitmap); 
+		if (bitmap == null) {
+			bitmap = Bitmap.createBitmap(c.getWidth(), c.getHeight(),
+					Bitmap.Config.ARGB_8888);
+			// canvas.drawRect(new Rect(c.getWidth()/2,0,0,0), p);
+			// canvas=new Canvas();
+			canvas.setBitmap(bitmap);
 
 		}
-		c.drawBitmap(bitmap, 0, 0, null);	   
-	}     
-		
+		c.drawBitmap(bitmap, 0, 0, null);
+	}
+
 }
--- src/motion/event/Gesture.java
+++ src/motion/event/Gesture.java
@@ -1,121 +1,143 @@
 package motion.event;
+
 import android.content.Context;
 import android.media.MediaPlayer;
 import android.util.AttributeSet;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
+import android.widget.ViewFlipper;
 import motion.event.Card;
-public class Gesture extends View implements android.view.GestureDetector.OnGestureListener{
+
+public class Gesture extends ViewFlipper implements
+		android.view.GestureDetector.OnGestureListener {
 	private MediaPlayer mediaPlayer;
-  	GestureDetector gd;  
-  	View layout;
-  	View relativeLayout;
-  	int i=0;
-  	
-  public Gesture(Context context, AttributeSet attrs){
-  	super(context, attrs);
-  	gd = new GestureDetector(this);  
-  	
-  }
+	GestureDetector gd;
+	View layout;
+	View relativeLayout;
+	int i = 0;
 
-  public boolean onTouchEvent(MotionEvent event) {  
-      // TODO Auto-generated method stub  
-      return this.gd.onTouchEvent(event);   
-  }
+	public Gesture(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		gd = new GestureDetector(this);
 
+	}
 
+	public boolean onTouchEvent(MotionEvent event) {
+		// TODO Auto-generated method stub
+		return this.gd.onTouchEvent(event);
+	}
 
-  @Override
-  public boolean onDown(MotionEvent arg0) {
-  	// TODO Auto-generated method stub
-  	
-  	return true;
-  }
+	@Override
+	public boolean onDown(MotionEvent arg0) {
+		// TODO Auto-generated method stub
 
+		return true;
+	}
 
+	@Override
+	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+			float velocityY) {
+		// TODO Auto-generated method stub
+		killMediaPlayer();
+		layout = ((View) this.getParent());
+		if ((e2.getX() - e1.getX()) > 10) {// 向左
+			i -= 1;
+			if (i < 0) {
+				i = Config.PICNAME.length - 1;
+			}
+			this.setInAnimation(AnimationUtils.loadAnimation(getContext(),
+					R.anim.push_right_in));
+			this.setOutAnimation(AnimationUtils.loadAnimation(getContext(),
+					R.anim.push_right_out));
+		} else if ((e1.getX() - e2.getX()) > 10) {// 向右
+			i += 1;
+			if (i > (Config.PICNAME.length - 1)) {
+				i = 0;
+			}
+			this.setInAnimation(AnimationUtils.loadAnimation(getContext(),
+					R.anim.push_left_in));
+			this.setOutAnimation(AnimationUtils.loadAnimation(getContext(),
+					R.anim.push_left_out));
+		}
+		Card c = (Card) layout.findViewById(R.id.dot);
+		c.clean();
 
-  @Override
-  public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
-  		float velocityY) {
-  	// TODO Auto-generated method stub
-	killMediaPlayer();
-  	layout = ((View) this.getParent());
-  	if((e2.getX()-e1.getX())>10){//向左
-  		i -= 1;
-  		if(i<0){
-  			i = Config.PICNAME.length-1;
-  		}
-  	}else if((e1.getX()-e2.getX())>10){//向右
-  		i +=1;
-  		if(i>(Config.PICNAME.length-1)){
-  			i = 0;
-  		}
-  	}
-  	Card c = (Card) layout.findViewById(R.id.dot);
-  	c.clean();
-  	
-  	relativeLayout = (View) layout.getParent();
-    ImageButton startPlayerBtn = (ImageButton)relativeLayout.findViewById(R.id.horn);
-    startPlayerBtn.setOnClickListener(new OnClickListener(){
-    	
-    	@Override
-    	public void onClick(View view){
-    		try{
-    			playLocalAudio(Config.SOUNDNAME[i]);
-   			
-    		}catch(Exception e){
-    			e.printStackTrace();
-    		}
-    	}
-    });
-  	layout.setBackgroundResource(Config.PICNAME[i]);
-   
-  	return false;
-  }
+		relativeLayout = (View) layout.getParent();
+		ImageButton startPlayerBtn = (ImageButton) relativeLayout
+				.findViewById(R.id.horn);
+		startPlayerBtn.setOnClickListener(new OnClickListener() {
 
-  @Override
-  public void onLongPress(MotionEvent arg0) {
-  	// TODO Auto-generated method stub
-  	
-  }
+			@Override
+			public void onClick(View view) {
+				try {
+					playLocalAudio(Config.SOUNDNAME[i]);
 
-  @Override
-  public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
-  		float distanceY) {
-  	// TODO Auto-generated method stub
-  	return false;
-  }
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		// this.removeViewAt(0);
+		this.addView(addImageById(Config.PICNAME[i]));
+		// this.addView(addImageById(R.drawable.p2));
+		// layout.setBackgroundResource(Config.PICNAME[i]);
+		this.showNext();
+		this.removeViewAt(0);
 
-  @Override
-  public void onShowPress(MotionEvent e) {
-  	// TODO Auto-generated method stub
-  	
-  }
+		return false;
+	}
 
-  @Override
-  public boolean onSingleTapUp(MotionEvent e) {
-  	// TODO Auto-generated method stub
-  	return false;
-  }
-  
-  private void playLocalAudio(int id)throws Exception
-  {
-  	mediaPlayer = MediaPlayer.create(getContext(), id);
-  	mediaPlayer.start();
-  }
-  private void killMediaPlayer(){
-	if(mediaPlayer!=null){
-		try{
-			mediaPlayer.release();
-		}catch(Exception e){
-			e.printStackTrace();
-		}	
-	}      	
-  }
-  public void setI(int j){
-	  i = j;
-  }
+	public View addImageById(int id) {
+		View iv = new View(getContext());
+		iv.setBackgroundResource(id);
+		return iv;
+	}
 
-  }
+	@Override
+	public void onLongPress(MotionEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
+			float distanceY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void onShowPress(MotionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean onSingleTapUp(MotionEvent e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	private void playLocalAudio(int id) throws Exception {
+		mediaPlayer = MediaPlayer.create(getContext(), id);
+		mediaPlayer.start();
+	}
+
+	private void killMediaPlayer() {
+		if (mediaPlayer != null) {
+			try {
+				mediaPlayer.release();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void setI(int j) {
+		i = j;
+	}
+
+}
